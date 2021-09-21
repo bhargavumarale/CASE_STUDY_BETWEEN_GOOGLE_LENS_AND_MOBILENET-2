5d4194a49dab5e3e3ce0af321aff69812629cccf
bhargavumarale <79691733+bhargavumarale@users.noreply.github.com>
2021-09-21 22:14:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+<center>
+        <h3 style="text-align: center;font-family: 'Montserrat', sen-serif;font-weight: bolder;color: red;"><span> CASE STUDY BETWEEN GOOGLE LENS AND MOBILNET</span></h3>
+        <br>
+        
+        <h3 style="text-align: center;font-family: 'Montserrat', sen-serif;font-weight: bolder;color: black;" >GOOGLE LENS <img src="gl.png" width="150px"></h3>
+        <h3 style="text-align: center;font-family: 'Montserrat', sen-serif;font-weight: bolder;color: red;" >VS</h4>
+        <h2 class="btn btn-primary h3" style="font-family: 'Prompt', sans-serif;">MOBILENET</h2>
+        <hr>
+        <div class="heading_div">
+              <ol><li><b style="color: white;" id="t">Test Image - </b></li></ol>
+          <br>
+          <img id="b" src="bottle.jpg">
+
+          <div class="btn btn-default output">
+            Output on <span class="span">Google Lens - </span> <b style="color: red;">Water Bottle</b>
+          </div>
+          
+          <div class="btn btn-default ou">
+            Output on <span class="span">MobileNet Model - </span> <b style="color: red;">Water Bottle</b>
+          </div>
+          <br>
+          <br>
+          <span>
+          <h4 id="i">Result - </h4>
+          </span>
+          <hr>
+        </div>
+    </center>
